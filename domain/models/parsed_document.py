019238ab7109b3bffc38121210c37e4ce407b01f
khurram-works <rajpootkhurramx82@gmail.com>
2026-07-18 20:10:26 +0500

    Improved Partitioning

--- domain/models/parsed_document.py
+++ domain/models/parsed_document.py
@@ -5,9 +5,5 @@ from domain.models.document_element import DocumentElement
 
 @dataclass(slots=True)
 class ParsedDocument:
-    profile: DocumentProfile
-    strategy: DocumentStrategy
-
     page_count: int
-
     elements: list[DocumentElement]
